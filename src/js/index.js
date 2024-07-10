7fd0799003508604d8a0dcf154cd8ebedf6e36d6
niko <nikolay17538583@gmail.com>
2024-07-10 18:07:21 +0300

    fix form openning script

--- src/js/index.js
+++ src/js/index.js
@@ -16,14 +16,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   securityItems.forEach((item) => {
     const checkbox = item.querySelector(".security__input");
+    const toggleButton = item.querySelector(".security__toggle");
+    const cancelButton = item.querySelector(".security__button-cancel");
     const body = item.querySelector(".security__body");
 
-    checkbox.addEventListener("change", function () {
-      if (checkbox.checked) {
-        body.style.display = "block";
-      } else {
-        body.style.display = "none";
-      }
+    toggleButton.addEventListener("click", function () {
+      checkbox.checked = true;
+      body.style.display = "block";
+      toggleButton.style.display = "none";
+    });
+
+    cancelButton.addEventListener("click", function () {
+      checkbox.checked = false;
+      body.style.display = "none";
+      toggleButton.style.display = "block";
     });
   });
 });
