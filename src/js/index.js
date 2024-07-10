23cd7e5b2ff3a26967d5d2654f546b55e6e54308
niko <nikolay17538583@gmail.com>
2024-07-10 16:52:28 +0300

    finally works

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const securityItems = document.querySelectorAll(".security__item");
+
+  securityItems.forEach((item) => {
+    const checkbox = item.querySelector(".security__input");
+    const body = item.querySelector(".security__body");
+
+    checkbox.addEventListener("change", function () {
+      if (checkbox.checked) {
+        body.style.display = "block";
+      } else {
+        body.style.display = "none";
+      }
+    });
+  });
+});
