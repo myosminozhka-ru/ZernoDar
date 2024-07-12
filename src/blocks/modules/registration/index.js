74ca7905976bea41e36f47a455e603401f408a84
niko <nikolay17538583@gmail.com>
2024-07-12 15:02:10 +0300

    add code input

--- src/blocks/modules/registration/index.js
+++ src/blocks/modules/registration/index.js
@@ -1,4 +1,4 @@
-export default function () {
+export default function signupSwitch() {
   document.getElementById("juridical").addEventListener("change", function () {
     document
       .getElementById("juridicalForm")
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,32 @@
 import "./import/modules";
+
+const inputs = document.querySelectorAll(".code-input");
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", () => {
+    if (input.value.length === 1) {
+      if (index !== inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    }
+  });
+
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Backspace") {
+      if (input.value.length === 0 && index !== 0) {
+        inputs[index - 1].focus();
+        inputs[index - 1].value = "";
+      } else {
+        input.value = "";
+      }
+    } else if (event.key.length === 1 && /\d/.test(event.key)) {
+      input.value = "";
+    }
+  });
+});
+
+inputs[inputs.length - 1].addEventListener("input", () => {
+  if (inputs[inputs.length - 1].value.length === 1) {
+    inputs[inputs.length - 1].blur();
+  }
+});
